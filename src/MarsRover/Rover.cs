3aa6b65819f1d285d135f125976601ddd37fff92
Garen Yondem <garen@yondem.com>
2019-04-03 00:10:03 +0300

    Add Rover object

---
+++ src/MarsRover/Rover.cs
@@ -0,0 +1,15 @@
+
+
+public class Rover
+{
+    private readonly IPoisition _position;
+    public IDirection Direction { get; private set; }
+    public Rover(IPoisition position)
+    {
+        _position = position;
+        Direction = new Direction(_position);
+    }
+    public void Move() => Direction.Move();
+    public void TurnLeft() => Direction.TurnLeft();
+    public void TurnRight() => Direction.TurnRight();
+}
