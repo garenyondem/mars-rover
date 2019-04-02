7d80a627958db5f8f88e676bf68b090c33cb75e9
Garen Yondem <garen@yondem.com>
2019-04-03 00:11:21 +0300

    Add entry point

---
+++ src/MarsRover/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mars_rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = new Grid(10, 5);
+            IPoisition initialPosition = new Position(grid);
+            var myRover = new Rover(initialPosition);
+            myRover.TurnLeft();
+            myRover.TurnLeft();
+            myRover.Move();
+            myRover.TurnRight();
+            Console.WriteLine(myRover.Direction.Name);
+
+            // TODO: Get current position info from Rover
+            // TODO: Prep parser for external commands
+        }
+    }
+}
