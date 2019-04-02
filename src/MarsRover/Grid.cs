d69f4e9941cbb1763d92767dab97b9aa1f191e7b
Garen Yondem <garen@yondem.com>
2019-04-03 00:07:21 +0300

    Add Grid model

---
+++ src/MarsRover/Grid.cs
@@ -0,0 +1,11 @@
+
+public class Grid
+{
+    public int width { get; private set; }
+    public int height { get; private set; }
+    public Grid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
